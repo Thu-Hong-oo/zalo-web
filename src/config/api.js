30282160c948bdcaa907d8201a6ae7f0a6a942e3
Thu-Hong-oo <thuhong05022003@gmail.com>
2025-05-30 15:57:37 +0700

    UPDATE url

--- src/config/api.js
+++ src/config/api.js
@@ -7,7 +7,7 @@ const COMPUTER_IP = "ec2-54-255-209-87.ap-southeast-1.compute.amazonaws.com";
 
 
 
-const BASE_URL = `http://${COMPUTER_IP}:3000`;
+const BASE_URL = `https://${COMPUTER_IP}:3000`;
 
 const API_URL = `${BASE_URL}/api`;
 const SOCKET_URL = BASE_URL;
